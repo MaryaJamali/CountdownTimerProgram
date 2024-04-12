102b27bf08b811da45e388f4324c89fe42028c84
MaryamJamali  ❤️ <150019136+MaryaJamali@users.noreply.github.com>
2024-04-12 13:55:30 +0330

    Coding the timeRemaining function

--- js/script.js
+++ js/script.js
@@ -6,12 +6,29 @@ const time = document.querySelector(".time");
 // Add event to button
 date.addEventListener("change", currentTime);
 
-function currentTime(t) {
+function currentTime(e) {
     // Prevent the default behavior of an event in the browser
-    t.preventDefault();
+    e.preventDefault();
     clearInterval(timeInterval);
     // Convert total time to milliseconds
     const temp = new Date(date.value);
     startTime(temp);
 };
 
+function startTime(d) {
+
+}
+
+function timeRemaining(d) {
+    let currentDate = new Date();
+    // Subtract the user's input time from the current time
+    let timeLeft = Date.parse(d) - Date.parse(currentDate);
+
+    // Algorithm to convert milliseconds to days, hours, minutes and seconds
+    let seconds = Math.floor((timeLeft / 1000) % 60);
+    let minutes = Math.floor(((timeLeft / 1000) / 60) % 60);
+    let hours = Math.floor((timeLeft / (1000 * 60 * 60)) % 24);
+    let days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+
+    return { "timeLeft": timeLeft, "days": days, "hours": hours, "minutes": minutes, "seconds": seconds };
+}
